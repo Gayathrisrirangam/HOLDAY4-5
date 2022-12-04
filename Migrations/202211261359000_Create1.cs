7a884e6b6851096fc17bd6d422e062f368c9dd9d
Gayathrisrirangam <101495614+Gayathrisrirangam@users.noreply.github.com>
2022-12-04 19:53:08 +0530

    Add files via upload

---
+++ Migrations/202211261359000_Create1.cs
@@ -0,0 +1,18 @@
+﻿namespace HOL4_project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Create1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Accounts", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Accounts", "Name", c => c.String());
+        }
+    }
+}
